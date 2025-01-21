025a7d6dec929b629ae8ba79bd5bfe159a74c092
David Silva Bazan <djsilvab@gmail.com>
2025-01-21 02:21:12 -0500

    Create_Tablas.sql

---
+++ BaezStone.Demo02.Bd/Create_Tablas.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Categoria(
+  CategoriaId int NOT NULL IDENTITY,
+  Nombre nvarchar(120) NOT NULL
+);
+
+ALTER TABLE Categoria
+ADD CONSTRAINT Categoria_PK PRIMARY KEY (CategoriaId);
+
+CREATE TABLE Marca(
+  MarcaId int NOT NULL IDENTITY,
+  Nombre nvarchar(120) NOT NULL
+);
+
+ALTER TABLE Marca
+ADD CONSTRAINT Marca_PK PRIMARY KEY (MarcaId);
+
+CREATE TABLE Producto(
+  ProductoId int NOT NULL IDENTITY,
+  Nombre nvarchar(120) NOT NULL,
+  Precio numeric(7,2) NOT NULL,
+  Costo numeric(7,2) NOT NULL,
+  CategoriaId int NOT NULL,
+  MarcaId int NOT NULL
+);
+
+ALTER TABLE Producto
+ADD CONSTRAINT Producto_PK PRIMARY KEY (ProductoId);
+
+ALTER TABLE Producto
+ADD CONSTRAINT Producto_Categoria_CategoriaId_FK FOREIGN KEY (CategoriaId)
+REFERENCES Categoria (CategoriaId) ON DELETE CASCADE;
+
+ALTER TABLE Producto
+ADD CONSTRAINT Producto_Marca_MarcaId FOREIGN KEY (MarcaId)
+REFERENCES Marca (MarcaId) ON DELETE CASCADE;
+
+INSERT INTO Categoria(Nombre) Values('Computadoras');
+INSERT INTO Categoria(Nombre) Values('Impresoras');
+
+INSERT INTO Marca(Nombre) Values('HP');
+INSERT INTO Marca(Nombre) Values('Apple');  
