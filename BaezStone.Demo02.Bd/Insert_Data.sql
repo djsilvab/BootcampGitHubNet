de9e4393946411d8ec6c36a688be988fb90585d3
David Silva Bazan <djsilvab@gmail.com>
2025-01-26 21:33:48 -0500

    Homologacion de scripts de SQL

--- BaezStone.Demo02.Bd/Insert_Data.sql
+++ BaezStone.Demo02.Bd/Insert_Data.sql
@@ -1,4 +1,14 @@
--- Producto --
+-- Data: Categoria --
+INSERT INTO Categoria(Nombre) Values('Computadoras'),('Impresoras');
+
+-- Data: Marca --
+INSERT INTO Marca(Nombre) Values('HP'),('Apple');
+
+-- Data: Producto --
 insert into Producto(Nombre,Precio,Costo,CategoriaId,MarcaId)
 values('Lenovo Thinkpad L480',2000,1500,1,1),
       ('HP - Envy',2000,1500,1,1);
+
+-- Data: PrecioOferta --
+INSERT INTO PrecioOferta(NuevoPrecio,TextoPromocional,ProductoId)
+VALUES(1800,'Oferta de verano',1);
